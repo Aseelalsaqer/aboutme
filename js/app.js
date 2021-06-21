b13d4f164a1f5402e82f81d99ed99db2e3479ef2
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-06-21 21:12:13 +0300

    adding aboutme

---
+++ js/app.js
@@ -0,0 +1,60 @@
+// 'use strict';
+// let name1 = prompt('What is your name?') 
+// alert('Welcome ' + name1)
+// console.log(name1)
+
+// let x = confirm('Get ready to know who I\'m')
+// console.log(x);
+// switch(x) {
+//     case true:
+//         alert('Put on your seat belt');
+//         break;
+//         case false:
+//             alert('You are already in :P');
+// }
+
+// let y = prompt('Are you intrested to know more about me?');
+// console.log(y);
+// switch (y.toUpperCase()){
+//     case 'YES':
+//         alert('I hope you enjoy it')
+//         break;
+//     case 'NO' :
+//             alert('Sorry, but you have to');
+
+        
+// }
+
+// let z = prompt('Do you think I\'m 24 years old?'); 
+// console.log(z);
+// switch (z.toUpperCase()){
+//     case 'YES':
+//         alert('When did I get this old :P ')
+//         break;
+//     case 'NO' :
+//             alert('I\'m 24'); 
+// }
+// let a = prompt('Can I be a web developer?'); 
+//  console.log(a);
+//  if (a == 'Yes'){
+//      alert('I hope So.');
+     
+//     } else if (a == 'No'){
+//         alert('Maybe I\'ll');
+//      }
+
+
+ 
+
+
+            
+
+
+
+
+
+
+
+
+
+
