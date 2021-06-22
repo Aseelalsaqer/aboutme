1729665e3df1782f4f4083e6e726ff542bd0a024
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-06-22 13:28:57 +0300

    editing JS

--- js/app.js
+++ js/app.js
@@ -1,48 +1,53 @@
-// 'use strict';
-// let name1 = prompt('What is your name?') 
-// alert('Welcome ' + name1)
-// console.log(name1)
+'use strict';
+let name1 = prompt('What is your name?') 
+alert('Welcome ' + name1)
+console.log(name1)
 
-// let x = confirm('Get ready to know who I\'m')
-// console.log(x);
-// switch(x) {
-//     case true:
-//         alert('Put on your seat belt');
-//         break;
-//         case false:
-//             alert('You are already in :P');
-// }
+let x = confirm('Get ready to know who I\'m')
+console.log(x);
+switch(x) {
+    case true:
+        alert('Put on your seat belt');
+        break;
+        case false:
+            alert('You are already in :P');
+}
 
-// let y = prompt('Are you intrested to know more about me?');
-// console.log(y);
-// switch (y.toUpperCase()){
-//     case 'YES':
-//         alert('I hope you enjoy it')
-//         break;
-//     case 'NO' :
-//             alert('Sorry, but you have to');
+let y = prompt('Are you intrested to know more about me?');
+console.log(y);
+switch (y.toUpperCase()){
+    case 'YES':
+    case 'Y' :   
+        alert('I hope you enjoy it')
+        break;
+    case 'NO' :
+    case 'N' :    
+            alert('Sorry, but you have to');
 
         
-// }
+}
 
-// let z = prompt('Do you think I\'m 24 years old?'); 
-// console.log(z);
-// switch (z.toUpperCase()){
-//     case 'YES':
-//         alert('When did I get this old :P ')
-//         break;
-//     case 'NO' :
-//             alert('I\'m 24'); 
-// }
-// let a = prompt('Can I be a web developer?'); 
-//  console.log(a);
-//  if (a == 'Yes'){
-//      alert('I hope So.');
+let z = prompt('Do you think I\'m 24 years old?'); 
+console.log(z);
+switch (z.toUpperCase()){
+    case 'YES':
+    case 'Y':    
+        alert('When did I get this old :P ')
+        break;
+    case 'NO' :
+    case 'N'  : 
+        alert('I\'m 24'); 
+}
+let a = prompt('Can I be a web developer?'); 
+ console.log(a);
+ if (a == 'Yes' || a == 'Y'){
+     alert('I hope So.');
      
-//     } else if (a == 'No'){
-//         alert('Maybe I\'ll');
-//      }
+    } else if (a == 'No' || a == 'N'){
+        alert('Maybe I\'ll');
+     }
 
+     alert ('Thank You  ' +   name1   + '  for your time ')
 
  
 
